abe3641a1713e9e81226aa10434ad6a4354acf04
graceaitken1 <grace.aitken@ada.ac.uk>
2024-09-25 13:42:30 +0000

    Refactoring of code

--- rps.js
+++ rps.js
@@ -3,85 +3,21 @@ function rockPaperScissors(player1, player2) {
     return "draw";
   }
 
-  if (player1 === "rock") {
-    if (player2 === "scissors") {
-      return "player1";
-    }
-  } else {
-    return "player2";
-  }
+const winningConditions = {
+  rock: ["scissors", "lizard"], 
+  scissors: ["paper", "lizard"], 
+  paper: ["rock", "spock"], 
+  lizard: ["spock", "paper"], 
+  spock: ["scissors", "rock"]
+};
 
-  if (player1 === "paper") {
-    if (player2 === "rock") {
-    } else {
-      return "player2";
-    }
-  }
-
-  if (player1 === "scissors") {
-    if (player2 === "paper") {
-      return "player1";
-    } else {
-      return "player2";
-    }
-  }
-
-  if (player1 === "scissors") {
-    if (player2 === "lizard") {
-      return "player1";
-    } else {
-      return "player2";
-    }
-  }
-
-  if (player1 === "rock") {
-    if (player2 === "lizard") {
-      return "player1";
-    } else {
-      return "player2";
-    }
-  }
-
-  if (player1 === "spock") {
-    if (player2 === "rock") {
-      return "player1";
-    } else {
-      return "player2";
-    }
-  }
-
-  if (player1 === "spock") {
-    if (player2 === "scissors") {
-      return "player1";
-    } else {
-      return "player2";
-    }
-  }
-
-  if (player1 === "lizard") {
-    if (player2 === "spock") {
-      return "player1";
-    } else {
-      return "player2";
-    }
-  }
-
-  if (player1 === "paper") {
-    if (player2 === "spock") {
-      return "player1";
-    } else {
-      return "player2";
-    }
-  }
-
-  if (player1 === "lizard") {
-    if (player2 === "paper") {
-      return "player1";
-    } else {
-      return "player2";
-    }
-  }
+if (winningConditions[player1]. includes(player2)) {
+  return "player1";
+} else { 
+  return "player2";
 }
+}
+
 
 // Leave this code here for the automated tests
 module.exports = {
