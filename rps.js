bdc63f99b8eedd17802103c77f24dce6a4862e08
graceaitken1 <grace.aitken@ada.ac.uk>
2024-09-25 14:06:11 +0000

    Refactor return statement

--- rps.js
+++ rps.js
@@ -3,21 +3,16 @@ function rockPaperScissors(player1, player2) {
     return "draw";
   }
 
-const winningConditions = {
-  rock: ["scissors", "lizard"], 
-  scissors: ["paper", "lizard"], 
-  paper: ["rock", "spock"], 
-  lizard: ["spock", "paper"], 
-  spock: ["scissors", "rock"]
-};
+  const winningCondition = {
+    rock: ["scissors", "lizard"],
+    scissors: ["paper", "lizard"],
+    paper: ["rock", "spock"],
+    lizard: ["spock", "paper"],
+    spock: ["scissors", "rock"],
+  };
 
-if (winningConditions[player1]. includes(player2)) {
-  return "player1";
-} else { 
-  return "player2";
+  return winningCondition[player1].includes(player2) ? "player1" : "player2"
 }
-}
-
 
 // Leave this code here for the automated tests
 module.exports = {
