5f977a59bc7ab8d3b0bebff0ad930cbc35c1f1b0
graceaitken1 <grace.aitken@ada.ac.uk>
2024-09-25 13:08:11 +0000

    Pass 2 tests

--- rps.js
+++ rps.js
@@ -1,15 +1,32 @@
 function rockPaperScissors(player1, player2) {
 
-  
-   
+if (player1 === player2) {
+  return "draw";
 }
 
+if (player1 === "rock") {
+  if (player2 === "scissors"){
+    return "player1";
+  }
+}   else { 
+  return "player2";
+} 
 
+if (player1 === "paper") {
+  if (player2 === "rock") {
+  } else { 
+    return "player2";
+  }
+}
 
-
-
-
-
+if (player1 === "scissors") {
+  if (player2 === "paper") { 
+    return "player1";
+  } else { 
+    return "player2";
+  }
+  }
+}
 
 
 
