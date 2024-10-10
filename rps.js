d55616d64ca54c4238e6d36cb5f310201627712a
graceaitken1 <grace.aitken@ada.ac.uk>
2024-10-10 13:08:47 +0000

    Final commit

--- rps.js
+++ rps.js
@@ -14,7 +14,6 @@ function rockPaperScissors(player1, player2) {
   return winningCondition[player1].includes(player2) ? "player1" : "player2" 
 }
 
-// Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
 };
